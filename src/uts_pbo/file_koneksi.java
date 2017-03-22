9fb71a24954b10cc14c3173cf863d8e0180ac2aa
nana karlina <widyastutikarlina@gmail.com>
2017-03-22 15:01:11 +0700

    finish

---
+++ src/uts_pbo/file_koneksi.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts_pbo;
+
+/**
+ *
+ * @author Iitutsa
+ */
+public class file_koneksi {
+
+    public static Object GetConnection() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
